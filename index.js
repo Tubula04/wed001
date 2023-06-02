d4cd1bf64c726241380fdcaa5c76afec9aa10c0c
Tubula04 <135316941+Tubula04@users.noreply.github.com>
2023-06-02 11:35:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console. log ()
+console. log ("hellow kenya")
+let name ="stephen";
+console. log("Stephen");
+B="L";
+C="4";
+let A=B+C
+console.log(A);
+let rate=0.3;
+rate=1;
+console.log(rate);
